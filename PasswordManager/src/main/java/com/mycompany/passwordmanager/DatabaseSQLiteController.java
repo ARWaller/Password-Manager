8642db9dc85a96694729f147683c3809e1bcd6e2
ARWaller <100862048+ARWaller@users.noreply.github.com>
2023-11-26 21:09:53 -0500

    Update DatabaseSQLiteController.java

--- PasswordManager/src/main/java/com/mycompany/passwordmanager/DatabaseSQLiteController.java
+++ PasswordManager/src/main/java/com/mycompany/passwordmanager/DatabaseSQLiteController.java
@@ -1,11 +1,5 @@
-//@author Anthony Waller
-//This class represents the controller for the FXML files representing the user
-//interface
-package com.mycompany.passwordmanager;
 
-public class DatabaseSQLiteController {
-    
-}
+package com.mycompany.passwordmanager;
 
 // 1. Database operations for storing username and encrypted password
 
@@ -15,24 +9,56 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String DATABASE_URL = "jdbc:sqlite:path_to_your_database/passwords.db";
-
+    private static final String URL = "jdbc:sqlite:src/main/resources/com/mycompany/passwordmanager/Accounts.db";
+    
     /**
      * Inserts a new record into the user_passwords table.
      *
      * @param username           The username associated with the password.
-     * @param encryptedPassword  The encrypted password to store.
+     * @param password  The encrypted password to store.
      */
-    public static void insertPassword(String username, String encryptedPassword) {
-        String sql = "INSERT INTO user_passwords(username, encrypted_password) VALUES(?,?)";
-
-        try (Connection conn = DriverManager.getConnection(DATABASE_URL);
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, username);
-            pstmt.setString(2, encryptedPassword);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+public static void insertPassword(String username, String password) throws SQLException{
+        try(Connection conn = DriverManager.getConnection(URL)){
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO Accounts(username, password,type) VALUES (?,?)");
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.executeUpdate();
+    } catch (SQLException e){
+        e.printStackTrace();
+    }
+  }
+    
+    public static void retrievePassword() throws SQLException{
+        try(Connection conn = DriverManager.getConnection(URL)){
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Accounts");
+            ResultSet resultSet = stmt.executeQuery();
+            
+            while(resultSet.next()){
+                String username = resultSet.getString("Username");
+                String password = resultSet.getString("Password");
+                
+                
+                System.out.println("Username: "+username+"\tPassword: "+password);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void updatePassword(String username, String newPassword, String type) throws SQLException{
+        try(Connection conn = DriverManager.getConnection(URL)){
+        PreparedStatement stmt = conn.prepareStatement("UPDATE Accounts SET Password = ? WHERE Username = ?");
+        
+        stmt.setString(1, newPassword);
+        stmt.setString(2, username);
+        int rowsUpdated = stmt.executeUpdate();
+        
+        if(rowsUpdated > 0){
+            System.out.println("Password updated successfully for username: " + username);
+        }else {
+            System.out.println("Username not found. Password not updated");
+        }
+        
         }
     }
 }
