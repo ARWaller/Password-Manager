e5032c7321c05cc3d418ceb015a968bd9e05b1df
ARWaller <100862048+ARWaller@users.noreply.github.com>
2023-11-26 21:03:31 -0500

    Added hash and salt  methods

--- PasswordManager/src/main/java/com/mycompany/passwordmanager/Algorithms.java
+++ PasswordManager/src/main/java/com/mycompany/passwordmanager/Algorithms.java
@@ -35,4 +35,46 @@ public class EncryptionUtil {
         byte[] encryptedByte = cipher.doFinal(plainText.getBytes());
         return Base64.getEncoder().encodeToString(encryptedByte);
     }
+
+    //This method generates a random salt
+    private static byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    /**
+     * hashes a password with salt.
+     *
+     * @param password     The plaintext password to be hashed.
+     * @param salt         The salt that will added on to the hash
+     * @return 
+     */
+    private static String hashPassword(String password, byte[] salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // Concatenate the password and salt
+            byte[] password_with_salt = concatenateArrays(password.getBytes(), salt);
+
+            // Hash the concatenated array
+            byte[] hash = digest.digest(password_with_salt);
+
+            // Encode the hash in base64 for storage
+            return Base64.getEncoder().encodeToString(hash);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //used to conactenate password array with salt array
+    private static byte[] concatenateArrays(byte[] array1, byte[] array2) {
+        byte[] result = new byte[array1.length + array2.length];
+        System.arraycopy(array1, 0, result, 0, array1.length);
+        System.arraycopy(array2, 0, result, array1.length, array2.length);
+        return result;
+    }
 }
